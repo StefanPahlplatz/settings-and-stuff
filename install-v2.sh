412c1a709743c67d165d8c6368e5c701c24ba2f0
Pahlplatz,Stefan S <stefan314@live.nl>
2018-04-01 15:43:04 +0200

    Init script clones repo

--- install-v2.sh
+++ install-v2.sh
@@ -3,8 +3,9 @@
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    sudo apt-get install -y dialog python3-dialog python-apt >/dev/null
-    sudo python3 scripts/script.py
+    sudo apt-get install -y dialog python3-dialog python-apt git >/dev/null
+    git clone https://github.com/StefanPahlplatz/settings-and-stuff.git setup 
+    sudo python3 setup/scripts/script.py
 else
     echo "You are offline, please get yourself some internet and try again."
     exit
